ab0f91414a324c529d284c26c633e6881702af4f
DFrancis84 <devinfrancis84@gmail.com>
2020-09-14 19:39:37 -0400

    added middle initial to user input

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,9 @@
 def get_user_input():
     first_name = input("What is your first name: ")
+    middle_initial = input("What is your middle initial: ")
     last_name = input("What is your last name: ")
 
-    print(f'Hello {first_name} {last_name}! Welcome to Code Louisville - Monday Night Python Class')
+    print(f'Hello {first_name} {middle_initial} {last_name}! Welcome to Code Louisville - Monday Night Python Class')
 
 
 def main():
